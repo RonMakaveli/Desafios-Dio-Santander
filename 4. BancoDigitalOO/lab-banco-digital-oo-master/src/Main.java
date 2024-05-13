e0e82ba3a2ddf810f3867d1b847d8b01f2920fe6
RonMakaveli <ronaald_gustavoo@hotmail.com>
2024-05-13 18:16:08 -0300

    refactor: aplicando atualizações propostas na classe Main.java

--- 4. BancoDigitalOO/lab-banco-digital-oo-master/src/Main.java
+++ 4. BancoDigitalOO/lab-banco-digital-oo-master/src/Main.java
@@ -1,18 +1,24 @@
-
 public class Main {
 
 	public static void main(String[] args) {
+		Banco banco = new Banco();
+		banco.setNome("Meu Banco");
+
 		Cliente venilton = new Cliente();
 		venilton.setNome("Venilton");
-		
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
+		venilton.setConta(cc);
+
+		banco.getClientes().add(venilton);
+
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
-		
+
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
-	}
 
+		banco.listarClientes();
+	}
 }
